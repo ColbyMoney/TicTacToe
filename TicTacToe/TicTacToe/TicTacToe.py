3ca5a4547c4f07a04a0fbbbb81b776e093185836
Colby Money <122302338+ColbyMoney@users.noreply.github.com>
2023-03-11 20:02:33 -0600

    complete the methods

--- TicTacToe/TicTacToe/TicTacToe.py
+++ TicTacToe/TicTacToe/TicTacToe.py
@@ -1,15 +1,25 @@
-print("Hello world")
+from array import *
+
+if __name__ == '__main__':
+    main()
 
 def main():
-    playGame()
+    print("im working")
+    game = newGame()
+    pring("im still working")
+    while gameOver() == False:
+        takeTurn(player)
+        didWin(player)
+        if player == "X":
+            player = "O"
+        else:
+            player = "X"
 
 class Player:
     player = "X"
-    wins = 0
 
 class Game:
-    row = ["_","_","_"]
-    board = row[3]
+    board = [["_","_","_"], ["_","_","_"], ["_","_","_"]]
 
     def newGame(self):
         for x in range(0,2):
@@ -17,20 +27,36 @@ class Game:
                 self.board[x][y] = "_"
                 player = "X"
 
+    def playGame():
+        print("im working")
+        game = newGame()
+        pring("im still working")
+        while gameOver() == False:
+            takeTurn(player)
+            didWin(player)
+            if player == "X":
+                player = "O"
+            else:
+                player = "X"
+
     def takeTurn(player):
         row = input("Select row: ")
         column = input("Select column: ")
         while board[row][column] != "_":
+            print("That spot is taken! Try another spot.\n")
+            row = input("Select row: ")
+            column = input("Select column: ")
+        board[row][column] = player
 
+    def gameOver(game):
+        return game.didWin("X") or game.didWin("O") or game.boardFull(game)
 
-
-        if board[row][column] == "_":
-            board[row][column] = player
-        else while board[row][column] != 
-
-
-    def playGame():
-        game = newGame()
+    def boardFull(game):
+        for x in range(0,2):
+            for y in range(0,2):
+                if board[x][y] == "_":
+                    return False;
+        return True;
 
     def didWin(player):
         if (board[0][0] == player and board[0][1] == player and board[0][2] == player or 
@@ -45,4 +71,3 @@ class Game:
             board[2][0] == player and board[1][1] == player and board[0][2] == player):
             return True;
         return False;
-    
